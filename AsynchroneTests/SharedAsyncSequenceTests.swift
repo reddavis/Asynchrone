46dd4139b15656b60f228a130717a0015e61160f
Red Davis <me@red.to>
2021-12-31 15:19:58 +0000

    Remove the delay to simplify tests

--- AsynchroneTests/SharedAsyncSequenceTests.swift
+++ AsynchroneTests/SharedAsyncSequenceTests.swift
@@ -1,68 +1,54 @@
-//
-//  SharedAsyncSequenceTests.swift
-//  AsynchroneTests
-//
-//  Created by Michal Zaborowski on 2021-12-27.
-//
-
 import XCTest
 @testable import Asynchrone
 
+
 final class SharedAsyncSequenceTests: XCTestCase {
+    
     private var stream: SharedAsyncSequence<AsyncStream<String>>!
 
+    // MARK: Setup
+    
     override func setUpWithError() throws {
-        let values = "abcd"
-            .map { String(describing: $0) }
-            .reduce([String]()) { values, next in
-                let new = (values.last ?? "") + next
-                return values + [new]
-        }
-
-        let stream = AsyncStream<String> { continuation in
-            values.enumerated().forEach { i, value in
-                let delay = TimeInterval(i) / 100.0
-
-                DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-                    if i == values.count - 1 {
-                        continuation.yield(value)
-                        continuation.finish()
-                    } else {
-                        continuation.yield(value)
-                    }
-                }
+        let values = [
+            "a",
+            "ab",
+            "abc",
+            "abcd"
+        ]
+        
+        self.stream = AsyncStream { continuation in
+            for value in values {
+                continuation.yield(value)
             }
+            continuation.finish()
         }
-
-        self.stream = stream.shared()
+        .shared()
     }
 
+    // MARK: Tests
+    
     func testSharedStreamShouldNotThrowExceptionAndReceiveAllValues() async throws {
-
         Task {
-            let valuesStream = try await self.stream.collect()
-            XCTAssertEqual(valuesStream[0], "a")
-            XCTAssertEqual(valuesStream[1], "ab")
-            XCTAssertEqual(valuesStream[2], "abc")
-            XCTAssertEqual(valuesStream[3], "abcd")
+            let values = try await self.stream.collect()
+            XCTAssertEqual(values[0], "a")
+            XCTAssertEqual(values[1], "ab")
+            XCTAssertEqual(values[2], "abc")
+            XCTAssertEqual(values[3], "abcd")
         }
 
         Task.detached {
-            var valuesStream: [String] = []
-            for await value in self.stream.eraseToAnyAsyncSequenceable() {
-                valuesStream.append(value)
-            }
-            XCTAssertEqual(valuesStream[0], "a")
-            XCTAssertEqual(valuesStream[1], "ab")
-            XCTAssertEqual(valuesStream[2], "abc")
-            XCTAssertEqual(valuesStream[3], "abcd")
+            let values = try await self.stream.collect()
+            XCTAssertEqual(values[0], "a")
+            XCTAssertEqual(values[1], "ab")
+            XCTAssertEqual(values[2], "abc")
+            XCTAssertEqual(values[3], "abcd")
         }
 
-        let valuesStream = try await self.stream.collect()
-
-        XCTAssertEqual(valuesStream[0], "a")
-        XCTAssertEqual(valuesStream[1], "ab")
-        XCTAssertEqual(valuesStream[2], "abc")
-        XCTAssertEqual(valuesStream[3], "abcd")
+        let values = try await self.stream.collect()
+        XCTAssertEqual(values.count, 4)
+        XCTAssertEqual(values[0], "a")
+        XCTAssertEqual(values[1], "ab")
+        XCTAssertEqual(values[2], "abc")
+        XCTAssertEqual(values[3], "abcd")
     }
 }
