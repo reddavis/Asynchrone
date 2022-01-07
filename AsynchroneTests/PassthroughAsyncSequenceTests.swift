1d7b3dd3875a8439b98273ca8305c6bc0b162cb6
Red Davis <me@red.to>
2022-01-07 15:29:38 +0000

    Add PassthroughAsyncSequence and ThrowingPassthroughAsyncSequence

---
+++ AsynchroneTests/PassthroughAsyncSequenceTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import Asynchrone
+
+
+final class PassthroughAsyncSequenceTests: XCTestCase {
+    
+    // Private
+    private var sequence: PassthroughAsyncSequence<Int>!
+    
+    // MARK: Setup
+    
+    override func setUpWithError() throws {
+        self.sequence = .init()
+    }
+    
+    // MARK: Tests
+    
+    func testSequence() async throws {
+        self.sequence.yield(0)
+        self.sequence.yield(1)
+        self.sequence.finish(with: 2)
+        
+        let values = await self.sequence.collect()
+        XCTAssertEqual(values, [0, 1, 2])
+    }
+}
---
+++ AsynchroneTests/ThrowingPassthroughAsyncSequenceTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+@testable import Asynchrone
+
+
+final class ThrowingPassthroughAsyncSequenceTests: XCTestCase {
+    
+    // Private
+    private var sequence: ThrowingPassthroughAsyncSequence<Int>!
+    
+    // MARK: Setup
+    
+    override func setUpWithError() throws {
+        self.sequence = .init()
+    }
+    
+    // MARK: Tests
+    
+    func testSequence() async throws {
+        self.sequence.yield(0)
+        self.sequence.yield(1)
+        self.sequence.finish(with: 2)
+        
+        let values = try await self.sequence.collect()
+        XCTAssertEqual(values, [0, 1, 2])
+    }
+    
+    func testSequenceThrows() async throws {
+        self.sequence.finish(throwing: TestError())
+        
+        await XCTAsyncAssertThrowsError {
+            try await self.sequence.collect()
+        }
+    }
+}
